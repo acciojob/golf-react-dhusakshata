6832f1fcbbb2aeb1e5dd0d4dcd66cb8d45c3f75d
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-09-20 11:00:26 +0530

    submitting code-Wed Sep 20 2023 11:00:25 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -26,9 +26,15 @@ class App extends Component {
 
     // bind ArrowRight keydown event
     componentDidMount() {
+      document.addEventListener("keydown",(e)=>{
+        if(e.keyCode===39){
+            this.setState({
+                ballPosition:{
+                    left:this.state.posi+5+ "px"},posi:this.state.posi + 5})
+                }
+            });
+        }
       
-    }
-
     render() {
         return (
             <div className="playground">
